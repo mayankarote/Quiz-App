4f0c9ce36892cc0879f9306e49d80a1ba4b9f69b
Mayanka-88 <70584944+Mayanka-88@users.noreply.github.com>
2021-09-28 22:06:08 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app">
         <Header />
+        <Switch>
+          {/* <Route path="/" exact>
+            <Home
+              name={name}
+              setName={setName}
+              fetchQuestions={fetchQuestions}
+            />
+          </Route>
+          <Route path="/quiz">
+            <Quiz
+              name={name}
+              questions={questions}
+              score={score}
+              setScore={setScore}
+              setQuestions={setQuestions}
+            />
+          </Route>
+          <Route path="/result">
+            <Result name={name} score={score} />
+          </Route> */}
+        </Switch>
       </div>
+      <Footer />
     </BrowserRouter>
   );
 }
