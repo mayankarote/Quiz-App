b4fdfed0f59716b8745cb1b279bd1120cb4aeb57
Mayanka-88 <70584944+Mayanka-88@users.noreply.github.com>
2021-09-28 14:47:13 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import "./App.css";
 // import img from "../public/";
 function App() {
   return (
-    <div style={{ backgroundImage: "url(../public/ques.png)" }}>Hello</div>
+    <div
+      className="app"
+      style={{ backgroundImage: "url(../public/ques.png)" }}
+    ></div>
+    // <div className="app" style={{ backgroundImage: "url(./ques.png)" }}></div>
   );
 }
 
